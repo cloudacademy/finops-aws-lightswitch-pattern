25340394c61dc80351e5f0e2db00b7122f7cc72c
Jeremy Cook <jeremy.cook@cloudacademy.com>
2023-12-18 16:55:50 +1300

    updated implementation to stop instances

--- lambda_function.py
+++ lambda_function.py
@@ -6,20 +6,20 @@ ec2 = boto3.resource('ec2')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# get a list of all instances
-all_instances = [i for i in ec2.instances.all()]
-
 def lambda_handler(event, context):
+    logger.info("stopping instances...")
 
-    #instances = ec2.instances.filter(Filters=filters)
-    # get instances with filter of running + with tag `Name`
-    instances = [i for i in ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}, {'Name':'tag:AutoOff', 'Values':['False']}])]
+    instance_filter = [
+        {'Name': 'instance-state-name', 'Values': ['running']},
+        {'Name':'tag:AutoOff', 'Values':['True']}
+    ]
 
-    # make a list of filtered instances IDs `[i.id for i in instances]`
-    # Filter from all instances the instance that are not in the filtered list
-    instances_to_delete = [to_del for to_del in all_instances if to_del.id not in [i.id for i in instances]]
+    instances = [i for i in ec2.instances.filter(Filters=instance_filter)]
 
-    # run over your `instances_to_delete` list and terminate each one of them
-    print("Instances to delete:")
-    for instance in instances_to_delete:
-        print(instance.id)
+    logger.info(f"instances to be stopped: {instances}")
+
+    for instance in instances:
+        logger.info(f"stopping instance: {instance.id}")
+        instance.stop()
+
+    logger.info("instances stopped")
